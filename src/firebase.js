05b2d7a51f3af978700240b422a6246286e17588
justmai1 <justmai@uw.edu>
2026-01-28 09:53:53 -0800

    firebase js webapp

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3lghTVKCLPCB8ZWOpE7D6khnv7VQ59_8",
+  authDomain: "water-works-15086.firebaseapp.com",
+  projectId: "water-works-15086",
+  storageBucket: "water-works-15086.firebasestorage.app",
+  messagingSenderId: "883260931965",
+  appId: "1:883260931965:web:bd212089525eed6c3c9614"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

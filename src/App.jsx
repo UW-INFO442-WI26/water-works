d969f35442fc9da40fe317596b3f6b7c8c8ba829
dangwilly <willdang@uw.edu>
2026-02-05 09:45:29 -0800

    Fixed file directory for browser

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router'
 <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet"></link>
-import { Map, Navbar } from './home';
+import { Map, Navbar } from './components/Map.jsx';
 
 function App() {
   return (
